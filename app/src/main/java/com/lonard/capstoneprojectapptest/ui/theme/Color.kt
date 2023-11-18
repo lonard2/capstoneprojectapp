161f67d40dc799a6aa3c52a06cc504a1e95d127d
Dimas Surya Wirastama <dimas.shie.jhoki@gmail.com>
2023-11-18 13:57:50 +0700

    nambahin color red dan blue

--- app/src/main/java/com/lonard/capstoneprojectapptest/ui/theme/Color.kt
+++ app/src/main/java/com/lonard/capstoneprojectapptest/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Menambahkan warna biru dan merah
+val blue = Color(0xFF0024FF)
+val red = Color(0xFFFF0015)
